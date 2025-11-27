7b21e0444ee0812867096228c60c02c4f7c89aa0
Navdeep <sharma.navdeep7404@gmail.com>
2025-11-27 08:36:58 +0530

    Update main.dart with Firebase init and app setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,10 @@
 import 'package:flutter/material.dart';
-// import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'login_screen.dart';
 
-// For desktop/UI testing, REMOVE Firebase initialization:
-void main() {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
-
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const ExamDutyApp());
 }
 
